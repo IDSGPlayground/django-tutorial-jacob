d43a54d38d3f1d733689fb7b19e04c179e513046
Jacobknew <jacobknew@gmail.com>
2012-07-10 16:16:32 -0700

    added some polls views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,22 @@
-from django.http import HttpResponse
+from django.template import Context, loader
+from django.http import HttpResponse, Http404
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    t = loader.get_template('polls/index.html')
+    c = Context({'latest_poll_list': latest_poll_list,})
+    return HttpResponse(t.render(c))
+
+def detail(request, poll_id):
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
