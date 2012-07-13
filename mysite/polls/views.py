2fca0e54a8d0159d5e7528969aae2515012a643f
Jacobknew <jacobknew@gmail.com>
2012-07-13 10:05:16 -0700

    Added the voting functionality and a results page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from polls.models import Poll, Choice
-from django.core.urlsolvers import reverse
+from django.core.urlresolvers import reverse
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -15,7 +15,8 @@ def detail(request, poll_id):
     return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
